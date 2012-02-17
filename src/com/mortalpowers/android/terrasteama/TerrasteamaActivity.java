4e35cf12d5882595622a3a6b71fabc117f129e40
Stephen Fluin <stephen.fluin@gmail.com>
2012-02-16 18:14:12 -0600

    Created Terrasteama

---
+++ src/com/mortalpowers/android/terrasteama/TerrasteamaActivity.java
@@ -0,0 +1,13 @@
+package com.mortalpowers.android.terrasteama;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class TerrasteamaActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
