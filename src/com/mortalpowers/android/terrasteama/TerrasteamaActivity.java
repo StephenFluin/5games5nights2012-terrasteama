389dba749eccf26b5eef1ba6b1eab363ef0812f7
Doug Reeves <guodman@gmail.com>
2012-02-16 19:06:41 -0600

    created a simple list view

--- src/com/mortalpowers/android/terrasteama/TerrasteamaActivity.java
+++ src/com/mortalpowers/android/terrasteama/TerrasteamaActivity.java
@@ -1,13 +1,26 @@
 package com.mortalpowers.android.terrasteama;
 
-import android.app.Activity;
-import android.os.Bundle;
+import java.util.ArrayList;
 
-public class TerrasteamaActivity extends Activity {
+import android.app.Activity;
+import android.app.ListActivity;
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+public class TerrasteamaActivity extends ListActivity {
+	private ArrayAdapter<String> adapter;
+	
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
+        ArrayList<String> items = new ArrayList<String>();
+        items.add("blah");
+        items.add("glorp");
+        adapter = new ArrayAdapter<String>(this, R.layout.list_item, items);
+        setListAdapter(adapter);
+        ListView lv = getListView();
+        lv.setTextFilterEnabled(true);
     }
 }
