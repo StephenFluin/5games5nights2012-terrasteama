80641bfed17772ae5973bf237b2f19e75a55a8e6
Doug Reeves <guodman@gmail.com>
2012-02-16 20:29:43 -0600

    added an upgrad dialog

--- src/com/mortalpowers/android/terrasteama/TerrasteamaActivity.java
+++ src/com/mortalpowers/android/terrasteama/TerrasteamaActivity.java
@@ -4,13 +4,21 @@ import java.util.ArrayList;
 import java.util.Vector;
 
 import android.app.Activity;
+import android.app.AlertDialog;
 import android.app.ListActivity;
+import android.content.Context;
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.TextView;
 
 public class TerrasteamaActivity extends ListActivity {
 	private ArrayAdapter<String> adapter;
+	
 	
     /** Called when the activity is first created. */
     @Override
@@ -25,5 +33,34 @@ public class TerrasteamaActivity extends ListActivity {
         setListAdapter(adapter);
         ListView lv = getListView();
         lv.setTextFilterEnabled(true);
+        lv.setOnItemClickListener(new UpgradeBuilding());
     }
+    
+	private class UpgradeBuilding implements OnItemClickListener {
+		public void onItemClick(AdapterView parent, View view, int position,
+				long id) {
+			System.out.println("Clicked");
+			AlertDialog.Builder builder = new AlertDialog.Builder(
+					TerrasteamaActivity.this);
+			String name = adapter.getItem(position);
+			builder.setMessage("Would you like to upgrade " + name + "?")
+					.setCancelable(false)
+					.setPositiveButton("Upgrade",
+							new DialogInterface.OnClickListener() {
+								public void onClick(DialogInterface dialog,
+										int id) {
+									TerrasteamaActivity.this.finish();
+								}
+							})
+					.setNegativeButton("Not Yet",
+							new DialogInterface.OnClickListener() {
+								public void onClick(DialogInterface dialog,
+										int id) {
+									dialog.cancel();
+								}
+							});
+			AlertDialog alert = builder.create();
+			alert.show();
+		}
+	}
 }
